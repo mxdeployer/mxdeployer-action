7fb3c79fd4d90712280ddb35aab60a1ef9943b82
Andy Sherwood <xinmyname@gmail.com>
2022-05-30 14:27:34 -0700

    Log host, stop logging payload

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+
+  const host = core.getInput("dest-host")
+  console.log(`Host: ${host}`);
+
   const egassem = core.getInput("message").split("").reverse().join("")
   console.log(`Message: ${egassem}`);
-
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
 
 } catch (error) {
   core.setFailed(error.message);
